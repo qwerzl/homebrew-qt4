3dab1a4e519437599fc88775973cb7e2c90e2edb
qwerzl <46770502+qwerzl@users.noreply.github.com>
2023-12-13 12:21:14 +0800

    Update qt@4.rb

--- qt@4.rb
+++ qt@4.rb
@@ -21,7 +21,6 @@ class QtAT4 < Formula
   deprecated_option "qtdbus" => "with-dbus"
   deprecated_option "with-d-bus" => "with-dbus"
 
-  depends_on "openssl@1.0"
   depends_on "dbus" => :optional
   depends_on "mysql" => :optional
   depends_on "postgresql" => :optional
